26d09e8c4fc434da8082527d7ae6575b1f353acc
Salim Salaues <ssalaues@student.42.us.org>
2017-07-03 11:52:04 -0700

    ex00

--- d00/megaphone.cpp
+++ d00/megaphone.cpp
@@ -6,21 +6,28 @@
 /*   By: ssalaues <ssalaues@student.42.us.org       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/03 10:37:12 by ssalaues          #+#    #+#             */
-/*   Updated: 2017/07/03 10:53:46 by ssalaues         ###   ########.fr       */
+/*   Updated: 2017/07/03 11:39:11 by ssalaues         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main(void)
+int	main(int ac, char **av)
 {
-	char	buff[512];
-
-	std::cout << "Hello World !" << std::endl;
-
-	std::cout << "Input a word: ";
-	std::cin >> buff;
-	std::cout << "You entered: [" << buff << "]" << std::endl;
-
+	if (ac > 1)
+	{
+		av++;
+		while(*av)
+		{
+			for(int i = 0; (*av)[i]; i++)
+			{
+				std::cout << (char)toupper((*av)[i]);
+			}
+			av++;
+		}
+		std::cout << '\n';
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 	return 0;
 }
