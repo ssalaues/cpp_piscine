42640311dcc28aee7930c8122804c441ace4d11c
Salim Salaues <ssalaues@student.42.us.org>
2017-07-07 14:12:40 -0700

    working on d03

---
+++ d03/ex00/FragTrap.hpp
@@ -0,0 +1,22 @@
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+#include <string>
+#include <iostream>
+
+class FragTrap
+{
+	public:
+			
+
+	private:
+			unsigned int	_hp;
+			unsigned int	_MaxHP;
+			unsigned int	_energy;
+			unsigned int	_maxEnergy;
+			unsigned int	_lvl;
+			unsigned int	_pName;
+			unsigned int	_meleeDmg;
+			unsigned int	_rangeDmg;
+			unsigned int	_armorRed;
+};
