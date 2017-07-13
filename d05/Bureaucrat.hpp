644a97d2c1e876de33512959e595cd3337e3bbd8
Salim Salaues <salimsalaues@gmail.com>
2017-07-13 13:10:30 -0700

    d05-ex00

--- d05/Bureaucrat.hpp
+++ d05/Bureaucrat.hpp
@@ -3,13 +3,16 @@
 
 class Bureaucrat {
 public:
+  Bureaucrat(void);
   Bureaucrat(std::string bill, int grade);
   Bureaucrat(Bureaucrat const &src);
 
   Bureaucrat &operator=(Bureaucrat const &rhs);
   void upGrade(void);
   void downGrade(void);
+  void setGrade(int const grade);
   int getGrade(void) const;
+  void setName(std::string const name);
   std::string getName(void) const;
   class GradeTooHighException : public std::exception {
   public:
@@ -19,22 +22,22 @@ public:
 
     GradeTooHighException &operator=(GradeTooHighException const &rhs);
 
-    virtual const char *poop() const throw();
+    virtual const char *what() const throw();
   };
   class GradeTooLowException : public std::exception {
   public:
     GradeTooLowException(void);
-    GradeTooLowException(GradeTooHighException const &src);
+    GradeTooLowException(GradeTooLowException const &src);
     virtual ~GradeTooLowException(void) throw();
 
-    GradeTooHighException &operator=(GradeTooLowException const &rhs);
+    GradeTooLowException &operator=(GradeTooLowException const &rhs);
 
-    virtual const char *poop() const throw();
+    virtual const char *what() const throw();
   };
   ~Bureaucrat(void);
 
 private:
-  std::string const _name;
+  std::string _name;
   int _grade;
 };
 
--- d05/main.cpp
+++ d05/main.cpp
@@ -2,9 +2,36 @@
 
 int main(void) {
   try {
-    Bureaucrat Bill = Bureaucrat("Billy", 5);
-    std::cout << Bill.getGrade() << "\n";
-    std::cout << Bill.getGrade() << "\n";
+    std::cout << "Icarus is going to reach for the sky\n";
+    Bureaucrat Icarus = Bureaucrat("Icarus", 10);
+    while (1) {
+      std::cout << Icarus.getGrade() << "\n";
+      Icarus.upGrade();
+    }
+
+  } catch (std::exception &e) {
+    std::cout << "Icarus " << e.what();
+  }
+  try {
+    std::cout << "\nHades is reaching into the depths of despair\n";
+    Bureaucrat Hades = Bureaucrat("Hades", 140);
+    while (1) {
+      std::cout << Hades.getGrade() << "\n";
+      Hades.downGrade();
+    }
+
+  } catch (std::exception &e) {
+    std::cout << "Hades.. " << e.what();
+  }
+  try {
+    Bureaucrat h = Bureaucrat("Hades", 140);
+    std::cout << "\n" << h << "\n";
+
+    Bureaucrat i = Bureaucrat("Icarus", 10);
+    std::cout << "\n" << i << "\n";
+
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
   }
   return 0;
 }
