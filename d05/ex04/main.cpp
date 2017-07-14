008fd29e4823793206eacc3ed09f421e37c3664d
Salim Salaues <salimsalaues@gmail.com>
2017-07-14 13:00:52 -0700

    main

--- d05/ex04/main.cpp
+++ d05/ex04/main.cpp
@@ -6,21 +6,7 @@
 #include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 
-int main(void) { /*
-   Bureaucrat gizmo = Bureaucrat("Gremlin", 40);
-   Intern furby;
-
-   Form *f1 = furby.makeForm("RobotomyRequest", "Bender");
-   gizmo.signForm(*f1);
-   Form *f2 = furby.makeForm("PresidentialPardon", "Bender");
-   Form *f3 = furby.makeForm("ShrubberyCreation", "Bender");
-
-   std::cout << "\n" << *f1 << "\n";
-   std::cout << *f2 << "\n";
-   std::cout << *f3 << "\n";
-
-   Form *f4 = furby.makeForm("Bad form", "Bender");
- */
+int main(void) {
   Intern idiotOne;
   Bureaucrat hermes = Bureaucrat("Hermes Conrad", 37);
   Bureaucrat bob = Bureaucrat("Bobby Bobson", 123);
