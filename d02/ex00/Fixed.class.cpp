8bdc5b6e560b77c5de8ae809ffafa5089735d185
Salim Salaues <ssalaues@student.42.us.org>
2017-07-24 10:33:02 -0700

    d02 from vog

---
+++ d02/ex00/Fixed.class.cpp
@@ -0,0 +1,40 @@
+#include "Fixed.class.hpp"
+
+Fixed::Fixed(void) : _n(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+	return;
+}
+
+Fixed::Fixed(Fixed const & src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+	return;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+	return;
+}
+
+Fixed & Fixed::operator=(Fixed const & rhs)
+{
+	std::cout << "Assignation operator called" << std::endl;
+
+	this->_n = rhs.getRawBits();
+	return *this;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_n;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->_n = raw;
+	return;
+}
---
+++ d02/ex01/Fixed.class.hpp
@@ -0,0 +1,31 @@
+#ifndef FIXED_CLASS_HPP
+# define FIXED_CLASS_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+
+	private:
+		int _n;
+		static const int _fractionalBits = 8;
+
+	public:
+		Fixed(void);
+		Fixed(Fixed const & src);
+		Fixed(int const n);
+		Fixed(float const n);
+		~Fixed(void);
+
+		Fixed & operator=(Fixed const & rhs);
+
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+
+		float toFloat(void) const;
+		int toInt( void ) const;
+};
+
+std::ostream & operator<<(std::ostream & o, Fixed const & rhs);
+
+#endif
